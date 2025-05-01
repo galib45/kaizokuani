58a3b87cc273d4ac84fa7763ae2e5228c8a987a5
Asadul Al Galib <asadulgalibvat69@gmail.com>
2025-05-01 12:46:02 +0600

    v2.3.2 - animeprogress

--- app/src/main/java/com/galib/kaizokuani/ui/screens/VideoPlaybackScreenComposable.kt
+++ app/src/main/java/com/galib/kaizokuani/ui/screens/VideoPlaybackScreenComposable.kt
@@ -6,22 +6,18 @@ import android.view.OrientationEventListener
 import android.view.WindowInsets
 import android.view.WindowInsetsController
 import android.view.WindowManager
-import android.widget.TextView
 import androidx.activity.compose.BackHandler
 import androidx.annotation.OptIn
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
@@ -31,14 +27,12 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableLongStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.AndroidView
@@ -52,12 +46,8 @@ import androidx.media3.exoplayer.ExoPlayer
 import androidx.media3.ui.PlayerView
 import androidx.navigation.NavHostController
 import com.galib.kaizokuani.R
-import com.galib.kaizokuani.data.AnimeProgress
 import com.galib.kaizokuani.data.AppDataManager
-import com.galib.kaizokuani.data.LastPlayedEpisode
 import com.galib.kaizokuani.ui.VideoPlaybackScreen
-import com.galib.ui.theme.AppTypography
-import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
 @OptIn(UnstableApi::class)
